0a5ea26c723df5002b81b61d0b1b84500af9be34
gary-pickens <gary_pickens@yahoo.com>
2013-08-01 09:21:54 -0500

    Trying more stuff

--- follows.js
+++ follows.js
@@ -14,7 +14,7 @@
 
 	var defaultKey		= 'ybwPH9462XIyemXo3ayvPSAzeJlOYlPMZihn1We3dybvRn73ybwPH9462XIyemXo3ayvPSAzeJlOYlPMZihn1We3dybvRn73', // Unique master Xively API key to be used as a default
 		defaultFeeds	= [64451], // Comma separated array of Xively Feed ID numbers
-		applicationName	= 'House Monitor', // Replaces Xively logo in the header
+		applicationName	= 'House Monitor!', // Replaces Xively logo in the header
 		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
 		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
