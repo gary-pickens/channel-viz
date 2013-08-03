8ef7dc3232a74342647e4ce1396d0c473b0876d4
gary-pickens <gary_pickens@yahoo.com>
2013-08-03 09:00:51 -0500

    More fixes to javascript

--- door-1.js
+++ door-1.js
@@ -21,20 +21,20 @@ function get_xively_data()
 		    	}		
 		    	
 		    document.getElementById("door").innerHTML = value;
-		})
-		    
-		xively.datastream.subscribe( feedID, datastreamID, function ( event , datastream_updated ) { 
-		    value = datastream_updated["current_value"] 
-		    if (value == 1)
-		    	{
-		    	value = "The door is closed"
-		    	}
-		    else
-		    	{
-		    	value = "The door is open"
-		    	}		
-		    	
-		    document.getElementById("door").innerHTML = value;
+
+		    xively.datastream.subscribe( feedID, datastreamID, function ( event , datastream_updated ) { 
+			    value = datastream_updated["current_value"] 
+			    if (value == 1)
+			    	{
+			    	value = "The door is closed"
+			    	}
+			    else
+			    	{
+			    	value = "The door is open"
+			    	}		
+			    	
+			    document.getElementById("door").innerHTML = value;
+		    })
 		})
 	})
 }
