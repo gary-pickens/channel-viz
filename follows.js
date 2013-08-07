dd4aa8018cfdc9ef100df2384087486351fa1af3
Gary Pickens <gary_pickens@yahoo.com>
2013-08-06 19:49:51 -0500

    Change width

--- follows.js
+++ follows.js
@@ -149,7 +149,7 @@
 						 			// Build Graph
 									var graph = new Rickshaw.Graph( {
 										element: document.querySelector('#graph-' + feedId + '-' + datastream.id),
-										width: 1000,
+										width: 600,
 										height: 600,
 										renderer: 'line',
 										min: parseFloat(datastream.min_value) - .25*(parseFloat(datastream.max_value) - parseFloat(datastream.min_value)),
