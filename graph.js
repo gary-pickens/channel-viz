eac16524ffc0c570f1f538813c51295b3e7e4d89
gary-pickens <gary_pickens@yahoo.com>
2013-08-08 18:47:03 -0500

    Changed to moment for date calculations

--- graph.js
+++ graph.js
@@ -55,11 +55,13 @@ screen_height = $(document).height();
 xively.setKey(xivelyKey);
 
 var duration = '1day';
-var graph_start = new Date();
+var graph_end = moment.utc();
+var graph_start = moment.utc()
+	graph_start.subtract('days', 7)
  
 var query = {
-	start: graph_start,
-	duration: duration,
+	start: graph_start.toJSON(),
+	stop: graph_end.toJSON(),
 	interval: 900,
 	limit: 1000
 };
