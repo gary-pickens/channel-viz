9de7c6e2a8306e138bf0a259e2628c4bc437d9b5
Gary Pickens <gary_pickens@yahoo.com>
2013-09-06 19:21:32 -0500

    Fixed bad merge

--- graph.js
+++ graph.js
@@ -126,11 +126,7 @@ function loadData(data) {
 	var filtedData = data.datapoints.filter(function(x) { return (x.value < 1000); });
 	for (var i=0; i < filtedData.length; i++ ) {
 		var utc = moment.utc(filtedData[i].at);
-<<<<<<< HEAD
-		var date = utc.subtract("hours", 6)
-=======
-		var date = utc.subtract("hours", 5)
->>>>>>> d8e5d2c5d83cb281932a4127c85db0294ddba388
+		var date = utc.subtract("hours", 5);
 		var value = parseInt(filtedData[i].value);
 		series[i] = {x: date.valueOf()/1000, y: value};
 	}
